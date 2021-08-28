3a312b9b2a80304d1c566105d701ae49db52d96a
Joshua Kim <44937149+joxhkim@users.noreply.github.com>
2021-08-28 12:23:03 -0500

    Create frontBack.java

---
+++ Java Warmup-1/frontBack.java
@@ -0,0 +1,14 @@
+/*Given a string, return a new string where the first and last chars have been exchanged.
+
+
+frontBack("code") → "eodc"
+frontBack("a") → "a"
+frontBack("ab") → "ba" */
+
+public String frontBack(String str) {
+  if (str.length() <= 1) {
+    return str;
+  }
+  String middle = str.substring(1, str.length()-1);
+  return str.charAt(str.length()-1) + middle + str.charAt(0);
+}
